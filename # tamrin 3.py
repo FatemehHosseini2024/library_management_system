7e9295a95b158b7c36644dfd2dcc91a340432e85
FatemehHosseini2024 <fh815946@gmail.com>
2024-09-17 01:38:13 +0330

    create library

--- # tamrin 3.py
+++ # tamrin 3.py
@@ -29,4 +29,31 @@ class Member:
     def return_book(self,book):
         self.borrowed_books.remove(book)
         book.return_book()
+class Library:
+   
+   
+    def __init__(self) :
+         books=[]
+         members=[]
+    def add_book(self,book):
+        self.books.append(book)
+    def register_member(self,member):
+        self.members.append(member)
+    def issue_book(self,member_id,isbn):
+        for member in self.members:
+            if member.member_id == member_id:
+                for book in self.books:
+                    if book.isbn == isbn:
+                        member.borrow_book(book)
+    def return_book(self,member_id,isbn):
+         for member in self.members:
+            if member.member_id == member_id:
+                for book in self.books:
+                    if book.isbn == isbn:
+                        member.return_book(book)
+
+
+
+
         
+
