c7983466373eb1d09a170b388a397c802586d21e
FatemehHosseini2024 <fh815946@gmail.com>
2024-09-17 00:56:43 +0330

    create member

--- # tamrin 3.py
+++ # tamrin 3.py
@@ -14,3 +14,19 @@ class Book:
         available =False
     def return_book(self):
         available =True
+class Member:
+    name="name"
+    member_id="M098"
+    borrowed_books=[]
+    def __init__(self,name,member_id) :
+        self.name=name
+        self.member_id=member_id
+    def __str__(self) :
+        return f"name : {self.name}, member id: {self.member_id}"
+    def borrow_book(self,book):
+        self.borrowed_books.append(book)
+        book.borrow()
+    def return_book(self,book):
+        self.borrowed_books.remove(book)
+        book.return_book()
+        
